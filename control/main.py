7ab767279272ba40865640739e833be6e4b10cb9
snjvn <91363279+snjvn@users.noreply.github.com>
2023-01-08 23:50:57 +0530

    Update main.py

--- control/main.py
+++ control/main.py
@@ -3,18 +3,20 @@ from commands import Pluto
 
 
 def main():
-	pluto = Pluto()
+    pluto = Pluto()
 
-	pluto.arm()
-	print("Taking Off")
-	pluto.takeoff()
-	# time.sleep(2)
-	# print("Increasing Throttle")
-	# pluto.rc(1500, 1500, 2000, 1500)
-	# time.sleep(2)
-	# print("Landing")
-	# pluto.land()
-	# pluto.disarm()
+    #pluto.arm()
+    pluto.get_alti()
+    #pluto.disarm()
+	#print("Taking Off")
+	#pluto.takeoff()
+	#time.sleep(2)
+	#print("Increasing Throttle")
+	#pluto.rc(1500, 1500, 2000, 1500)
+	#time.sleep(2)
+	#print("Landing")
+	#pluto.land()
+    
 
 if __name__ == '__main__':
 	main()
