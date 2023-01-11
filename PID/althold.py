c0c571bc61552e0e634fa5a14a6c53252bf4edf5
hrishikesh2002 <200010037@iitdh.ac.in>
2023-01-11 18:23:25 +0530

    althold class created

--- PID/althold.py
+++ PID/althold.py
@@ -3,46 +3,44 @@ from position.arucoDetection import arucoDetection
 import control
 from time import time
 
-# class AltitudeHold:
-#     def __init__():
+class AltitudeHold:
+    def __init__(self, dist, estimator=arucoDetection()):
         
-# Create a position object
+        self.position = estimator
+        while not self.position.setOrigin(1):
+            print("Setting origin")
+            pass
+        self.drone = control.Pluto()
+        self.distance = dist
+        
+        
+    def setupPID(self, Kp=0.1, Ki=0.5, Kd=0.01, sample_time=0.01):
+                
 
-position = arucoDetection("http://10.196.4.192:8080/shot.jpg")
-position.setOrigin(1)
+        self.pid = PID(Kp, Ki, Kd, setpoint=self.distance)
+        self.pid.sample_time = sample_time
+        
 
-pos, ori = position.getPose(1)
-x, y, z = pos[0], pos[1], pos[2]
-
-# Parameter Tuning
-
-Kp = 0.1
-Kd = 0.5
-Ki = 0.01
-
-
-
-pid = PID(Kp, Ki, Kd, setpoint=0)
-pid.sample_time = 0.01
-
-# Set the required height of the drone
-
-pid.setpoint = 0.5 # in meters
-
-drone = control.Pluto()
-
-drone.arm()
-
-start = time()
-
-while time() < start + 60:
-    pos, ori = position.getPose(1)
-    x, y, z = pos[0], pos[1], pos[2]
-    output = pid(z)
     
-    drone.rc(1500, 1500, 1000 + output, 1500)
-    
-drone.disarm()
+    def althold(self, duration):
+        
+        self.drone.arm()
+        
+        start = time()
+        
+        while time() < start + duration:
+            pos = self.position.getPose(1)
+            if len(pos)==0:
+                break
+            ori, (x, y, z) = pos
+            output = self.pid(z)
+            
+            self.drone.rc(1500, 1500, 1000 + output, 1500)
+        
+        self.drone.disarm()
+            
+            
+ 
 
 
 
